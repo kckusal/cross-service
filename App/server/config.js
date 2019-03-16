9362b10caf9572d153ddc1f29f295f004d75d846
Kusal KC <kckusal99@gmail.com>
2019-03-16 18:11:09 +0300

    Create server configuration file

---
+++ App/server/config.js
@@ -0,0 +1,34 @@
+const config = {
+    development: {
+        //url to be used in link generation
+        url: 'http://my.site.com',
+        //mongodb connection settings
+        database: {
+            host:   '127.0.0.1',
+            port:   '27017',
+            db:     'site_dev'
+        },
+        //server details
+        server: {
+            host: '127.0.0.1',
+            port: '3422'
+        }
+    },
+    production: {
+        //url to be used in link generation
+        url: 'http://my.site.com',
+        //mongodb connection settings
+        database: {
+            host: '127.0.0.1',
+            port: '27017',
+            db:     'site'
+        },
+        //server details
+        server: {
+            host:   '127.0.0.1',
+            port:   '3421'
+        }
+    }
+};
+
+module.exports = config;
